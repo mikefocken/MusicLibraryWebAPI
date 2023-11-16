81a9159bc88fa7c89f3adcf02200340102458611
mike focken <mikefocken@gmail.com>
2023-11-16 15:35:24 -0600

    save work

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryBackendProject.Models;
+
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibraryBackendProject.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
